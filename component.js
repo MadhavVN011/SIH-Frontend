bb6674fee361b782b24eff92f15bd5528e436d0c
Madhav Nair <140394990+MadhavVN011@users.noreply.github.com>
2024-09-09 14:08:11 +0530

    Add files via upload

--- component.js
+++ component.js
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/w2ECctGG59q
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu"
